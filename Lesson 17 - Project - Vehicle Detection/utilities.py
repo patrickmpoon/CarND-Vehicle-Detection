cfad721e180d42505148739248f8902511ee50ee
Patrick Poon <ppoon@demandware.com>
2017-04-22 13:26:07 -0400

    Code abstraction and clean up

---
+++ Lesson 17 - Project - Vehicle Detection/utilities.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+from skimage.feature import hog
+
+
+# Define a function to compute binned color features
+# def bin_spatial(img, size=(32, 32)):
+#     # Use cv2.resize().ravel() to create the feature vector
+#     features = cv2.resize(img, size).ravel()
+#     # Return the feature vector
+#     return features
+
+def bin_spatial(img, size=(32, 32)):
+    color1 = cv2.resize(img[:,:,0], size).ravel()
+    color2 = cv2.resize(img[:,:,1], size).ravel()
+    color3 = cv2.resize(img[:,:,2], size).ravel()
+    return np.hstack((color1, color2, color3))
+
+
+# Define a function to compute color histogram features
+# NEED TO CHANGE bins_range if reading .png files with mpimg!
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:, :, 0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:, :, 1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:, :, 2], bins=nbins, range=bins_range)
+
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+
+
+def convert_color(img, conv='RGB2YCrCb'):
+    if conv == 'RGB2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    if conv == 'BGR2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    if conv == 'RGB2LUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # make a copy of the image
+    draw_img = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(draw_img, bbox[0], bbox[1], color, thick)
+    # draw each bounding box on your image copy using cv2.rectangle()
+    # return the image copy with boxes drawn
+    return draw_img # Change this line to return image copy with boxes
+
+
+# Define a function to return HOG features and visualization
+def get_hog_features(img,
+                     orient,
+                     pix_per_cell,
+                     cell_per_block,
+                     vis=False,
+                     feature_vec=True):
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient,
+                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block),
+                                  transform_sqrt=True,
+                                  visualise=vis,
+                                  feature_vector=feature_vec)
+        return features, hog_image
+
+    # Otherwise call with one output
+    else:
+        features = hog(img, orientations=orient,
+                       pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block),
+                       transform_sqrt=True,
+                       visualise=vis,
+                       feature_vector=feature_vec)
+        return features
+
